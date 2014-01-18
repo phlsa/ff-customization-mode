39de2d3e540c9cff3f7fbcd3e9da822201de0e0e
Philipp Sackl <philipp@mozilla.com>
2014-01-18 09:58:39 +0100

    initial commit

---
+++ prototype/js/app.js
@@ -0,0 +1,16 @@
+$('.panel').click(function(e) {
+	$('.window').toggleClass('customize');
+});
+
+// delay each icon
+var icons = $('.button');
+// clamp the delay if there are many icons
+var transitionDelay = 0.1;
+var maxTransitionTime = 0.7;
+if ( transitionDelay * (icons.length-1) > maxTransitionTime ) {
+	transitionDelay = maxTransitionTime/(icons.length-1);
+}
+console.log(transitionDelay);
+_.each(icons, function(item, index) {
+	$(item).css({'transition-delay': 0.3+transitionDelay*index+'s'});
+});
