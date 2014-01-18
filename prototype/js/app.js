babebdd9bc0654b0f6ae0be9f090165f20fe8516
Philipp Sackl <philipp@mozilla.com>
2014-01-18 15:54:37 +0100

    Better bouncing

--- prototype/js/app.js
+++ prototype/js/app.js
@@ -2,15 +2,14 @@ $('.panel').click(function(e) {
 	$('.window').toggleClass('customize');
 });
 
-// delay each icon
-var icons = $('.button');
-// clamp the delay if there are many icons
-var transitionDelay = 0.1;
-var maxTransitionTime = 0.7;
-if ( transitionDelay * (icons.length-1) > maxTransitionTime ) {
-	transitionDelay = maxTransitionTime/(icons.length-1);
-}
-console.log(transitionDelay);
-_.each(icons, function(item, index) {
-	$(item).css({'transition-delay': 0.3+transitionDelay*index+'s'});
-});
+// // delay each icon
+// var icons = $('.button');
+// // clamp the delay if there are many icons
+// var transitionDelay = 0.1;
+// var maxTransitionTime = 0.7;
+// if ( transitionDelay * (icons.length-1) > maxTransitionTime ) {
+// 	transitionDelay = maxTransitionTime/(icons.length-1);
+// }
+// _.each(icons, function(item, index) {
+// 	$(item).css({'transition-delay': 0.3+transitionDelay*index+'s'});
+// });
